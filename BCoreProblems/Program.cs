bab55c21b88b62bb68e8c518e3f71262a77e80ac
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-28 14:34:41 +0530

    Welcome

---
+++ BCoreProblems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BCoreProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
