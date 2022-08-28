0bf4cb0d67a50303579b718c2ece3f81c729e76f
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-28 15:10:32 +0530

    [Vinaya]Add: HarmonicNumber

---
+++ HarmonicNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramA
+{
+    public class HarmonicNumber
+    {
+        public void Harmonic()
+        {
+            int i, n;
+            double sum = 0.0;
+
+            Console.WriteLine("Enter the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            // (1/1 +1/2 +..... 1/n)
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("1/{0} + ", i);
+                sum += 1 / (float)i;
+            }
+            Console.WriteLine("\nSum of Series upto {0} terms : {1} \n", n, sum);
+        }
+    }
+}
